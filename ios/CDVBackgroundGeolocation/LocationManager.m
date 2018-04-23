cdf8992bd81634431a24f96e75ebac3c5a8a907c
Laurence Thompson <dlt.thompson@gmail.com>
2018-04-24 09:12:11 +0930

    OSC - changed showLocationSetting to NOOP

--- ios/CDVBackgroundGeolocation/LocationManager.m
+++ ios/CDVBackgroundGeolocation/LocationManager.m
@@ -312,7 +312,7 @@ enum {
 
 - (void) showLocationSettings
 {
-    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"prefs:root=LOCATION_SERVICES"]];
+    // NOOP - Since Apple started rejecting apps using non public url schemes
 }
 
 - (NSMutableDictionary*) getStationaryLocation
